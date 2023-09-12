8eb5e90715ba44c8221400752e8d86b19262b81c
francesca <francipeco36@gmail.com>
2023-09-12 15:29:33 +0200

    file preparation

---
+++ src/main/java/com/year/bestof/BestofApplication.java
@@ -0,0 +1,13 @@
+package com.year.bestof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestofApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/year/bestof/BestofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.year.bestof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
